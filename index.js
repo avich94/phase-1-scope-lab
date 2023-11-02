3bb6de6bfe0def05d1555851b0a3a1ab9e99e75c
Austin Vichich <avichich4@gmail.com>
2023-11-02 10:21:24 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+customerName();
+upperCaseCustomerName();
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+bestCustomer();
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+bestCustomer();
+
+
+const leastFavoriteCustomer = "tim";
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = "tod"; 
+  leastFavoriteCustomer = "tod";
+}
+
+changeLeastFavoriteCustomer();
